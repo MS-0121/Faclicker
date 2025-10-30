f2bba8bc2ff1521cba96d806cdfa051a5842ea74
門田行弘 <yukihiro271828@gmail.com>
2025-10-30 17:00:58 +0900

    メニューの実装 資源ノードの実装 日本語フォントの実装

---
+++ Assets/Script/ButtonHide.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//コミット
+
+public class ButtonHide : MonoBehaviour
+{
+    [SerializeField] 
+    [NamedArrayAttribute(new string[] { "鉄", "銅", "錫", "石炭", "金", "硫黄", "水", "粘土", "砂", "石膏", "方解石"})]
+    GameObject[] VeinButton = new GameObject[5];
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hide();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick(){
+        Hide();
+    }
+
+    public void Hide(){
+        for(int i = 0;i < VeinButton.Length;i++){
+            if(VeinButton[i].activeSelf) {
+                VeinButton[i].SetActive(false);
+            }else{
+                VeinButton[i].SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Script/TextManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+//コミット
+
+public class TextManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI ResourceName; 
+    [SerializeField] private GameManager GM;
+    int nowVein = 0;
+    string[] VeinName = { "鉄", "銅", "錫", "石炭", "金", "硫黄", "水", "粘土", "砂", "石膏", "方解石"};
+
+    [SerializeField]
+    [NamedArrayAttribute(new string[] { "鉄", "銅", "錫", "石炭", "金", "硫黄", "水", "粘土", "砂", "石膏", "方解石"})]
+    private TextMeshProUGUI[] VeinButtonText = new TextMeshProUGUI[11];
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResourcenameChange();
+        VeinNameChange();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ResourcenameChange();
+        VeinNameChange();
+    }
+
+    public void ResourceChange(int a){
+        //GameManagerで使う
+        nowVein = a;
+    }
+
+    public void ResourcenameChange(){
+        ResourceName.text = VeinName[nowVein]+"："+GM.Resource[nowVein];
+    }
+
+    public void VeinNameChange(){
+        for(int i = 0;i < 11;i++){
+            VeinButtonText[i].text = VeinName[i]+"：１/Click　生産量：１/s";
+        }
+    }
+}
